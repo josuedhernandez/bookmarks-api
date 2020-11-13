eca1111d4f5a1fd32e672a62cd52d66374cdf7c4
Josue Hernandez <moradel82@gmail.com>
2020-11-13 11:48:54 -0700

    Refactored code following TDD and alternate solution

--- src/bookmark/bookmark-router.js
+++ src/bookmark/bookmark-router.js
@@ -1,7 +1,8 @@
 const express = require("express");
 
 const { v4: uuid } = require("uuid");
-const bookmarks = require("../bookmarks-data");
+const data = require("../bookmarks-data");
+const { isWebUri } = require('valid-url')
 const logger = require("../logger");
 const bookmarkRouter = express.Router();
 const bodyParser = express.json();
@@ -9,29 +10,36 @@ const bodyParser = express.json();
 bookmarkRouter
   .route("/bookmarks")
   .get((req, res) => {
-    res.json(bookmarks);
+    res.json(data.bookmarks);
   })
   .post(bodyParser, (req, res) => {
-    const { title, description, ranking, url } = req.body;
+    const { title, description, rating, url } = req.body;
     if (!title) {
       logger.error(`Title is required`);
-      return res.status(400).send("Invalid data");
+      return res.status(400).send("'title' is required");
     }
-    if (!description) {
-      logger.error(`Description is required`);
-      return res.status(400).send("Invalid data");
-      436;
+    // Originally defined every field to be required.
+    // Refactored code applying TDD
+    // if (!description) {
+    //   logger.error(`Description is required`);
+    //   return res.status(400).send("Invalid data");
+    // }
+
+    if (!rating) {
+      logger.error(`Rating is required`);
+      return res.status(400).send(`'rating' is required`);
+    } else if (rating && (isNaN(rating) || rating < 1 || rating > 5)) {
+      logger.error(`'Rating' must be a number between 0 and 5`);
+      return res.status(400).send("'rating' must be a number between 0 and 5");
     }
-    if (!ranking) {
-      logger.error(`Ranking is required`);
-      return res.status(400).send("Invalid data");
-    } else if (ranking && (isNaN(ranking) || ranking < 1 || ranking > 5)) {
-      logger.error(`Ranking must be between 1 and 5`);
-      return res.status(400).send("Invalid data");
+
+    if(!url){
+        logger.error(`Rating is required`);
+        return res.status(400).send(`'url' is required`);
     }
-    if (!url) {
-      logger.error(`Url is required`);
-      return res.status(400).send("Invalid data");
+    if (!isWebUri(url)) {
+      logger.error(`Invalid url '${url}' supplied`)
+      return res.status(400).send(`'url' must be a valid URL`)
     }
     // get an id
     const id = uuid();
@@ -40,11 +48,11 @@ bookmarkRouter
       id,
       title,
       description,
-      ranking,
+      rating,
       url,
     };
 
-    bookmarks.push(bookmark);
+    data.bookmarks.push(bookmark);
     logger.info(`Bookmark with id ${id} created`);
 
     res
@@ -57,7 +65,7 @@ bookmarkRouter
   .route("/bookmarks/:id")
   .get((req, res) => {
     const { id } = req.params;
-    const bookmark = bookmarks.find((b) => b.id == id);
+    const bookmark = data.bookmarks.find((b) => b.id == id);
 
     // make sure we found a bookmark
     if (!bookmark) {
@@ -69,13 +77,13 @@ bookmarkRouter
   })
   .delete((req, res) => {
     const { id } = req.params;
-    const bookmarkIndex = bookmarks.findIndex((b) => b.id == id);
+    const bookmarkIndex = data.bookmarks.findIndex((b) => b.id == id);
 
     if (bookmarkIndex === -1) {
       logger.error(`Bookmark with id ${id} not found.`);
-      return res.status(404).send("Not found");
+      return res.status(404).send("Bookmark Not Found");
     }
-    bookmarks.splice(bookmarkIndex, 1);
+    data.bookmarks.splice(bookmarkIndex, 1);
     logger.info(`Bookmark with id ${id} deleted.`);
 
     res.status(204).end();
--- src/bookmarks-data.js
+++ src/bookmarks-data.js
@@ -1,11 +1,19 @@
-const boookmarks = [
+const bookmarks = [
   {
     id: 1,
-    title: "Task One",
-    description: "This is card one",
-    ranking: 5,
+    title: "Bookmark One",
+    description: "This is bookmark one",
+    rating: 2,
     url: "https://www.test.com",
+  },
+
+  {
+    id: 2,
+    title: "Bookmark Two",
+    description: "This is bookmark two",
+    rating: 5,
+    url: "https://www.test2.com",
   },
 ];
 
-module.exports = boookmarks;
+module.exports = { bookmarks } ;
